e93bc084fbf2e9acab28bebae6b6b79068870036
ftsnowy <daniil.championat@gmail.com>
2024-03-06 20:50:14 +0300

    l2_2

---
+++ DB2/lab2_2.sql
@@ -0,0 +1,57 @@
+CREATE OR REPLACE TRIGGER check_unique_id
+BEFORE INSERT ON STUDENTS
+FOR EACH ROW
+DECLARE
+    existing_id NUMBER;
+BEGIN
+    SELECT 1 INTO existing_id
+    FROM STUDENTS
+    WHERE ID = :NEW.ID;
+
+    IF existing_id IS NOT NULL THEN
+        RAISE_APPLICATION_ERROR(-20001, '??????? ? ????? ID ??? ??????????');
+    END IF;
+END;
+/
+
+CREATE OR REPLACE TRIGGER check_unique_id_groups
+BEFORE INSERT ON GROUPS
+FOR EACH ROW
+DECLARE
+    existing_id NUMBER;
+BEGIN
+    SELECT 1 INTO existing_id
+    FROM GROUPS
+    WHERE ID = :NEW.ID;
+
+    IF existing_id IS NOT NULL THEN
+        RAISE_APPLICATION_ERROR(-20002, '?????? ? ????? ID ??? ??????????');
+    END IF;
+END;
+/
+
+CREATE SEQUENCE STUDENTS_SEQ START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER generate_student_id
+BEFORE INSERT ON STUDENTS
+FOR EACH ROW
+BEGIN
+    :NEW.ID := STUDENTS_SEQ.NEXTVAL;
+END;
+/
+
+CREATE OR REPLACE TRIGGER check_unique_group_name
+BEFORE INSERT ON GROUPS
+FOR EACH ROW
+DECLARE
+    existing_name NUMBER;
+BEGIN
+    SELECT 1 INTO existing_name
+    FROM GROUPS
+    WHERE NAME = :NEW.NAME;
+
+    IF existing_name IS NOT NULL THEN
+        RAISE_APPLICATION_ERROR(-20003, '?????? ? ????? ?????? ??? ??????????');
+    END IF;
+END;
+/
